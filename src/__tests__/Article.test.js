a1fda89c46f260d58e5e9808a940c6a998e7ac31
Sayfudin <Seifdin62@gmail.com>
2024-10-30 21:45:58 +0300

    finishing Assignment

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,9 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  // Add this line to debug and see the current DOM
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
+
