12b43ef8a1ed183de0aa3d326948f4eb0e0c67b5
Sayfudin90 <Seifdin62@gmail.com>
2025-01-14 22:13:35 +0300

    submitting assignment

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,10 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
-  render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+    render(<Article />);
+    screen.debug(); // This will log the DOM to the console
+    expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
